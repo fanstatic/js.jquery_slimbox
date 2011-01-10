3613bca31dd89433e93cc98af6f64d900f6e618a
Jan-Jaap Driessen <devnull@localhost>
2011-01-10 19:38:10 +0100

    Copy the hurry.slimbox registration.

--- js/jquery_slimbox/__init__.py
+++ js/jquery_slimbox/__init__.py
@@ -1,9 +1,10 @@
-from fanstatic import Library, Resource
-import js.jquery
+from fanstatic import Library, Resource, GroupResource
+from js.jquery import jquery
 
 library = Library('slimbox', 'resources')
 
 slimbox_css = Resource(library, 'css/slimbox2.css')
 
-slimbox = Resource(library, 'src/slimbox2.js', minified='js/slimbox2.js',
-    depends=[js.jquery.jquery, slimbox_css])
+slimbox_js = Resource(library, 'js/slimbox2.js', depends=[jquery])
+
+slimbox = GroupResource([slimbox_css, slimbox_js])
